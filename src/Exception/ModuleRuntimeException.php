d9bdf1bdcbe05119d3250a03c33643be524e4a2a
steelice <im@steelice.info>
2023-09-17 21:50:28 +0200

    exception

--- src/Exception/ModuleRuntimeException.php
+++ src/Exception/ModuleRuntimeException.php
@@ -4,5 +4,8 @@ namespace Rulezdev\RulezbotBundle\Exception;
 
 class ModuleRuntimeException extends \RuntimeException
 {
-
+    public function __construct(string $message, public readonly bool $isClearWorkflow = false)
+    {
+        parent::__construct($message, 0, null);
+    }
 }
